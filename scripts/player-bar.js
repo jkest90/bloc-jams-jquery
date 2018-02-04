b865b069d800df98da24d4dc9af4cc63d8018940
jkest90 <jkest618@gmail.com>
2018-02-03 22:09:32 -0700

    assignment-219: complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,5 +1,3 @@
-/*-------------CHECKPOINT 219------------------>
------------------------------------------------*/
 
 {
     // select button with ID of play-pause and add a click event.
@@ -16,10 +14,21 @@
 
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
         const nextSongIndex = currentSongIndex + 1;
-        if (nextSongIndex >= album.songs.length) { return; }
+        if (nextSongIndex >= album.songs.length) { return; };
 
         const nextSong = album.songs[nextSongIndex];
         player.playPause(nextSong);
+    });
+
+    $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') { return; }
+
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = currentSongIndex - 1;
+        if (prevSongIndex < 0) { return; };
+
+        const prevSong = album.songs[prevSongIndex];
+        player.playPause(prevSong);
     })
 
 }
